f19250fa73f483e714ee423d55ef17ac86a8fc75
Stefan Nguyen <stefan@vinova.sg>
2013-09-23 23:15:08 +0700

    implement EntitySystem

--- spec/artemis/entity_system_spec.rb
+++ spec/artemis/entity_system_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Artemis::EntitySystem do
-  
+  it "inherits from EntityObserver" do
+    described_class < Artemis::EntityObserver 
+  end
+
+  describe "#initialize" do
+    it "clone all given aspect's bitsets" do
+      @aspect = Artemis::Aspect.new_for_all Artemis::Component
+      @system = Artemis::EntitySystem.new @aspect
+
+      [:all, :exclude, :one].each do |bitset|
+        bitset = "#{bitset}_set".to_sym
+        @system.send(bitset) == @aspect.send(bitset)
+      end
+    end
+  end
+
 end
