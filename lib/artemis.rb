dff6bd1551f82e626dfbbf0cd14f3f93ae0b5d16
Alex <alex@vinova.sg>
2013-09-23 21:07:44 +0700

    Add TagManager

--- lib/artemis.rb
+++ lib/artemis.rb
@@ -9,5 +9,8 @@ require 'artemis/world'
 require 'artemis/manager'
 require 'artemis/entity_manager'
 require 'artemis/group_manager'
+require 'artemis/tag_manager'
+
+require 'artemis/aspect'
 
 puts "Required artemis"
--- spec/artemis/aspect_spec.rb
+++ spec/artemis/aspect_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe Artemis::Aspect do
   
 end
