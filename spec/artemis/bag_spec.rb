b01f61891378d71dedf023ebc4d9080f47c744ab
Alex <alex@vinova.sg>
2013-09-24 12:09:34 +0700

    Cleanup

--- spec/artemis/bag_spec.rb
+++ spec/artemis/bag_spec.rb
@@ -1,42 +1,23 @@
 require 'spec_helper'
 
 describe Artemis::Bag do
-  before do
-    @world = double('world',
-      entity_manager: "entity_manager stub",
-      component_manager: "component_manager stub"
-    )
-  end
-
   it "work as exepected" do
     b = Artemis::Bag.new
     b.size.should == 0
 
+    w = Artemis::World.new
     s = 'abc'
-    e = Artemis::Entity.new(@world)
+    e = Artemis::Entity.new(w)
 
     b.add(s, 1)
     b.add(e)
 
     b.size.should == 2
-    b.should be_contains(s)
-    b.should be_contains(e)
+    b.values.should == [s, e]
 
-    b.each_entity.to_a.should == [s, e]
-
-    b.remove(3).should == false
-    b.remove(s).should == true
+    b.remove(3).should == b
+    b.remove(s).should == b
     b.should == {e.id => e}
-
-    b1 = Artemis::Bag.new
-    b1.add(s, 1)
-    b1.add(5, 10)
-    
-    b.add_all(b1)
-    b.values.should == [e, s, 5]
-
-    b.remove_all(b1)
-    b.should == {e.id => e}
-end
-
+    b.remove(e).should == e
+  end
 end
