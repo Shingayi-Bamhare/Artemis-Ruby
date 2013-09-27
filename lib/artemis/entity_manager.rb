7439fd740b3590ade038a1e4484ca09ce67eb257
Stefan Nguyen <stefan@vinova.sg>
2013-09-27 09:48:57 +0700

    fix type 'pus' -> 'puts' in entity_manager.rb

--- lib/artemis/entity_manager.rb
+++ lib/artemis/entity_manager.rb
@@ -43,7 +43,7 @@ module Artemis
         @deleted
       else
         @entities.remove(e)
-        pus "remove #{e} from EM's @entities list"
+        puts "remove #{e} from EM's @entities list" if @world.debug
         @disabled.delete(e.id)
         @active -= 1
         @deleted += 1
